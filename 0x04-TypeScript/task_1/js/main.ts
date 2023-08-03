b97616325109ac40f674218c1d2116b2ec8406d9
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-08-03 23:58:23 +0100

    wrote some typescript interfaces and classes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,7 +15,6 @@ export interface Teacher {
   [key: string]: any;
 }
 
-
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -33,3 +32,86 @@ console.log(teacher3);
 // fullTimeEmployee: false
 // lastName: "Doe"
 // location: "London"
+
+/*
+Write an interface named Directors that extends Teacher.
+It requires an attribute named numberOfReports(number)
+*/
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// should print
+// Object
+// firstName: "John"
+// fullTimeEmployee: true
+// lastName: "Doe"
+// location: "London"
+// numberOfReports: 17
+
+/*
+Write a function printTeacher:
+
+    It accepts two arguments firstName and lastName
+    It returns the first letter of the firstName and the full lastName
+    Example: printTeacher("John", "Doe") -> J. Doe
+
+Write an interface for the function named printTeacherFunction.
+*/
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+const teacher: string = printTeacher('Boomni', 'Jonathan');
+console.log(teacher);
+
+/*
+Write a Class named StudentClass:
+
+    The constructor accepts firstName(string) and lastName(string) arguments
+    The class has a method named workOnHomework that return the string Currently working
+    The class has a method named displayName. It returns the firstName of the student
+    The constructor of the class should be described through an Interface
+    The class should be described through an Interface
+*/
+interface IStudentConstructor {
+  new (firstName: string, lastName: string): StudentClass;
+}
+
+interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements IStudentClass {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,26 +1,121 @@
 /*
-Write an interface named Directors that extends Teacher.
-It requires an attribute named numberOfReports(number)
-*/
-import {Teacher} from '../../task_1/js/main';
+Create the DirectorInterface interface with the 3 expected methods:
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+    workFromHome() returning a string
+    getCoffeeBreak() returning a string
+    workDirectorTasks() returning a string
+
+Create the TeacherInterface interface with the 3 expected methods:
+
+    workFromHome() returning a string
+    getCoffeeBreak() returning a string
+    workTeacherTasks() returning a string
+
+Create a class Director that will implement DirectorInterface
+
+    workFromHome should return the string Working from home
+    getToWork should return the string Getting a coffee break
+    workDirectorTasks should return the string Getting to director tasks
+
+Create a class Teacher that will implement TeacherInterface
+
+    workFromHome should return the string Cannot work from home
+    getCoffeeBreak should return the string Cannot have a break
+    workTeacherTasks should return the string Getting to work
+
+Create a function createEmployee with the following requirements:
+
+    It can return either a Director or a Teacher instance
+    It accepts 1 arguments:
+        salary(either number or string)
+    if salary is a number and less than 500 - It should return a new Teacher. Otherwise it should return a Director
+
+Expected result:
+
+console.log(createEmployee(200));
+Teacher
+console.log(createEmployee(1000));
+Director
+console.log(createEmployee('$500'));
+Director
+*/
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
 
-// should print
-// Object
-// firstName: "John"
-// fullTimeEmployee: true
-// lastName: "Doe"
-// location: "London"
-// numberOfReports: 17
+class Director implements DirectorInterface {
+  workFromHome() {
+    return "Working from home"
+  };
+  getCoffeeBreak() {
+    return "Getting a coffee break";
+  }
+  workDirectorTasks() {
+  return "Getting to director tasks";
+  }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome() {
+      return "Cannot work from home";
+    }
+    getCoffeeBreak() {
+      return "Cannot have a break";
+    }
+    workTeacherTasks() {
+      return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === "number" && salary < 500) { 
+    return new Teacher;
+  }
+  
+  return new Director;
+}
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
+
+/*
+Write a function isDirector:
+
+    it accepts employee as an argument
+    it will be used as a type predicate and if the employee is a director
+
+Write a function executeWork:
+
+    it accepts employee as an argument
+    if the employee is a Director, it will call workDirectorTasks
+    if the employee is a Teacher, it will call workTeacherTasks
+
+Expected result:
+
+executeWork(createEmployee(200));
+Getting to work
+executeWork(createEmployee(1000));
+Getting to director tasks
+*/
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee instanceof Director);
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
