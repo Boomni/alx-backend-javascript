981b163d59e450c440808c1a4d6a17174ff8c49a
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-19 20:35:11 +0000

    refractored code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,15 @@
 // Using Process stdin
 
-const readline = require("readline")
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-function ask(question) {
-  rl.question(question, (answer) => {
-    rl.write(`Your name is: ${answer}\n`);
-    rl.write("This important software is now closing\n");
-    rl.close();
-  });
-}
-
-ask("Welcome to Holberton School, what is your name?\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
