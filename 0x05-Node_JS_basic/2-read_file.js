62b77839282268deda187800530d953a81aac129
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-19 21:41:15 +0000

    Implemented function for reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+// Reading a file synchronously with Node JS
+const fs = require('fs');
+
+function countStudents(path) {
+  if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+
+  const data = fs.readFileSync(path, 'utf8').trim().split('\n');
+  const students = {};
+
+  for (const line of data.slice(1)) {
+    const [firstName, , , field] = line.split(',');
+    if (field) {
+      students[field] = students[field] || [];
+      students[field].push(firstName.trim());
+    }
+  }
+  console.log(`Number of students: ${data.length - 1}`);
+  
+  for (const field in students) {
+    const count = students[field].length;
+    const list = students[field].join(', ');
+    console.log(`Number of students in ${field}: ${count}. List: ${list}`);
+  }
+}
+
+module.exports = countStudents;
+
