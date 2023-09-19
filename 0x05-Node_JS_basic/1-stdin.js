c80bb6b96508e6ea65a0b64581b446089ea990a6
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-19 20:31:52 +0000

    refractored code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,16 +3,16 @@
 const readline = require("readline")
 
 const rl = readline.createInterface({
-  input: process.stdin, 
+  input: process.stdin,
   output: process.stdout,
-})
+});
 
 function ask(question) {
   rl.question(question, (answer) => {
-  rl.write(`Your name is: ${answer}\n`)
-  rl.write('This important software is now closing\n');
-  rl.close();
-  })
+    rl.write(`Your name is: ${answer}\n`);
+    rl.write("This important software is now closing\n");
+    rl.close();
+  });
 }
 
-ask("Welcome to Holberton School, what is your name?\n")
+ask("Welcome to Holberton School, what is your name?\n");
