91d95576b3ab2f9c4a0b75d69742cae309471d51
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-19 19:59:40 +0000

    Implemented a function using process stdin in nodejs

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+// Using Process stdin
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+  input: process.stdin, 
+  output: process.stdout,
+})
+
+function ask(question) {
+  rl.question(question, (answer) => {
+  rl.write(`Your name is: ${answer}\n`)
+  rl.write('This important software is now closing\n');
+  rl.close();
+  })
+}
+
+ask("Welcome to Holberton School, what is your name?\n")
