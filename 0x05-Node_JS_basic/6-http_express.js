c222cbfe584c2f4f797b22403294048bbb7eb3ec
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-24 16:41:09 +0000

    Created a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send(
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Error</title>
+</head>
+<body>
+  <pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;
