48361e9f733930eaf69eee2f4945c1bc19889f6f
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-24 17:02:20 +0000

    Created a more complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+const countStudents = (path, callback) => {
+  fs.readFile(path, 'utf8', (error, data) => {
+    if (error) {
+      callback(new Error('Cannot load the database'), null);
+    } else {
+      const lines = data.trim().split('\n').filter(Boolean);
+
+      if (lines.length === 0) {
+        callback(new Error('Cannot load the database'), null);
+      }
+
+      const students = {};
+
+      for (const line of lines.slice(1)) {
+        const [firstName, , , field] = line.split(',');
+        if (field) {
+          students[field] = students[field] || [];
+          students[field].push(firstName.trim());
+        }
+      }
+
+      let response = `Number of students: ${lines.length - 1}`;
+      for (const field in students) {
+        if ({}.hasOwnProperty.call(students, field)) {
+          const count = students[field].length;
+          const list = students[field].join(', ');
+          response += `\nNumber of students in ${field}: ${count}. List: ${list}`;
+        }
+      }
+
+      callback(null, response);
+    }
+  });
+};
+
+app.get('/students', (req, res) => {
+  countStudents('database.csv', (error, data) => {
+    if (error) {
+      res.statusCode = 500;
+      res.send(`Internal Server Error: ${error.message}`);
+    } else {
+      res.send(`This is the list of our students\n${data}`);
+    }
+  });
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send(
+    `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Error</title>
+</head>
+<body>
+  <pre>Cannot GET ${req.url}</pre>
+</body>
+</html>`,
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;
