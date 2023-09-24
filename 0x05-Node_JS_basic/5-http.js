a8d441c013bcc3f483a9d05d2e1db21d11c799fc
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-09-24 15:49:29 +0000

    Debugged eslint errors and debugged errors on the endpoints

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,68 @@
-// Small HTTP server using Node's HTTP module
-// Routing practice
 const http = require('http');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 1245;
+const db = process.argv[2];
+
+const countStudents = (path, callback) => {
+  fs.readFile(path, 'utf8', (error, data) => {
+    if (error) {
+      callback(new Error('Cannot load the database'), null);
+    } else {
+      const lines = data.trim().split('\n').filter(Boolean);
+
+      if (lines.length === 0) {
+        callback(new Error('Cannot load the database'), null);
+      }
+
+      const students = {};
+
+      for (const line of lines.slice(1)) {
+        const [firstName, , , field] = line.split(',');
+        if (field) {
+          students[field] = students[field] || [];
+          students[field].push(firstName.trim());
+        }
+      }
+
+      let response = `Number of students: ${lines.length - 1}\n`;
+
+      for (const field in students) {
+        if ({}.hasOwnProperty.call(students, field)) {
+          const count = students[field].length;
+          const list = students[field].join(', ');
+          response += `Number of students in ${field}: ${count}. List: ${list}`;
+        }
+      }
+
+      callback(null, response);
+    }
+  });
+};
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(db, (error, data) => {
+      if (error) {
+        res.statusCode = 500;
+        res.end(`Internal Server Error: ${error.message}`);
+      } else {
+        res.end(`This is the list of our students:\n ${data}`);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found\n');
+  }
 });
- 
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-app.get('/', function (req, res) {
-  res.send('root')
-})
-app.get('/students', function (req, res) {
-  res.end('This is the list of our students')
-})
-const countStudents = require('./3-read_file_async');
 
-countStudents("database.csv")
-    .then(() => {
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-module.exports = app
+module.exports = app;
